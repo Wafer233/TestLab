591a6e2d35e4a0af1f70089ae70559b552eae143
loacl_wafer <wangzefeng2333@gmail.com>
2025-03-03 15:40:39 +0900

    buildin, learn diff of new and make

---
+++ buildins/buildin_test.go
@@ -0,0 +1,27 @@
+package buildins
+
+import "testing"
+
+func TestAppend(t *testing.T) {
+	Append()
+}
+
+func TestLen(t *testing.T) {
+	Len()
+}
+
+func TestPrint(t *testing.T) {
+	Print()
+}
+
+func TestPanic(t *testing.T) {
+	Panic()
+}
+
+func TestNew(t *testing.T) {
+	New()
+}
+
+func TestMake(t *testing.T) {
+	Make()
+}
