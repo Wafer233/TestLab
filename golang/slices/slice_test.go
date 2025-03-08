882c86c8a397bb663de3409698b900c4e5ca37d0
loacl_wafer <wangzefeng2333@gmail.com>
2025-03-08 21:35:58 +0900

    ✨ Update slices.Contains, slices.Sort

--- golang/slices/slice_test.go
+++ golang/slices/slice_test.go
@@ -8,4 +8,5 @@ func TestContain(t *testing.T) {
 
 func TestSort(t *testing.T) {
 	Sort()
+
 }
--- golang/slices/slices.go
+++ golang/slices/slices.go
@@ -9,6 +9,7 @@ func Contain() {
 	numbers := []int{0, 1, 2, 3}
 	fmt.Println(slices.Contains(numbers, 2))
 	fmt.Println(slices.Contains(numbers, 4))
+
 }
 
 func Sort() {
