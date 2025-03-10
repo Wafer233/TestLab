e69d773d12d6f492f657c4d660dc8407c1bf6aa1
loacl_wafer <wangzefeng2333@gmail.com>
2025-03-10 13:25:29 +0900

    ✨ Update 0310

--- algo/slidingWindow.go
+++ algo/slidingWindow.go
@@ -1,7 +1,21 @@
 package algo
 
 //209. Minimum Size Subarray Sum
+/*
+	left = 0
+	right = 0
 
+	while right < len(nums):
+		window.append(nums[right])
+
+		while 窗口需要缩小:
+			# ... 可维护答案
+			window.popleft()
+			left += 1
+
+		# 向右侧增大窗口
+		right += 1
+*/
 func minSubArrayLen(target int, nums []int) int {
 	left := 0
 	right := 0
